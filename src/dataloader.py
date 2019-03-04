a8ef745ff5a96c9ad27ac6d6c47c6df301f67a37
Sumer Singh <sumer1337@gmail.com>
2019-03-04 17:41:48 -0500

    Followed PEP8

--- src/dataloader.py
+++ src/dataloader.py
@@ -4,58 +4,74 @@ import imageio
 
 
 def to to_one_hot(array, num_classes=3):
-	"""This function coverts the labels into a one hot vector.
+    """
+    This function coverts the labels into a one hot vector.
 
-	Keyword arguments:
-	array -- The numpy array of labels(masks) of shape (number_samples, img_width, img_height).
-	num_classes -- The number of classes (default 3)
+    Parameters
+    ----------
+    array : numpy array
+        The numpy array of labels(masks) of
+        shape (number_samples, img_width, img_height).
+    num_classes : int, optional
+        The number of classes (the default is 3)
 
-	Returns:
-	array_one_hot -- Numpy array of shape (number_samples, img_width, img_height, number_classes).
-	"""
+    Returns
+    -------
+    array_one_hot : numpy array
+        Numpy array of
+        shape (number_samples, img_width, img_height, number_classes).
+    """
 
-	sess = tf.Session(); 
-	array_one_hot = []
-	with sess.as_default():
-	    for i in range(len(array)):
-	        array_one_hot.append(tf.one_hot(array[i], num_classes).eval())
-	return array_one_hot
+    sess = tf.Session()
+    array_one_hot = []
+    with sess.as_default():
+        for i in range(len(array)):
+            array_one_hot.append(tf.one_hot(array[i], num_classes).eval())
+    return array_one_hot
 
 
 def load_data(training_list, testing_list):
-	"""This function loads the data required for the network.
+    """
+    This function loads the data required for the network.
 
-	Keyword arguments:
-	training_list -- List of training file names.
-	testing_list -- List of testing file names. 
+    Parameters
+    ----------
+    training_list : List
+        List of training file names.
+    testing_list : List
+        List of testing file names.
 
-	Returns:
-	X_train -- Numpy array of shape (number__train_samples, img_width, img_height, number_features).
-	X_test -- Numpy array of shape (number_test_samples, img_width, img_height, number_features).
-	y_train -- Numpy array of shape (number_training_samples, img_width, img_height, number_classes).
-	"""
+    Returns
+    -------
+    X_train : numpy array
+        Training data of
+        shape (number_train_samples, img_width, img_height, number_features).
+    X_test : numpy array
+        Testing data of
+        shape (number_train_samples, img_width, img_height, number_features).
+    y_train : numpy array
+        Training labels of
+        shape (number_training_samples, img_width, img_height, number_classes).
+    """
 
+    X_train = []
+    for index, name in enumerate(training_list):
+        current = np.load('features/' + name + '.npy')
+        if len(current.shape) < 3:
+            current = np.expand_dims(current, axis=-1)
+        X_train.append(current)
 
-	X_train = []
-	for index, name in enumerate(training_list):
-		current = np.load('features/' + name + '.npy')
-		if len(current.shape) < 3:
-			current = np.expand_dims(current, axis=-1)
-	    X_train.append(current)
+    X_test = []
+    for index, name in enumerate(testing_list):
+        current = np.load('features/' + name + '.npy')
+        if len(current.shape) < 3:
+            current = np.expand_dims(current, axis=-1)
+        X_test.append(current)
 
-	X_test = []
-	for index, name in enumerate(testing_list):
-		current = np.load('features/' + name + '.npy')
-		if len(current.shape) < 3:
-			current = np.expand_dims(current, axis=-1)
-	    X_test.append(current)
+    y_2d = []
+    for index, name in enumerate(training_list):
+        y_2d.append(imageio.imread('masks/' + name + '.png'))
 
-	y_2d = []
-	for index, name in enumerate(training_list):
-		y_2d.append(imageio.imread('masks/' + name + '.png'))
+    y_train = to_one_hot(y_2d)
 
-	y_train = to_one_hot(y_2d)
-
-	return X_train, X_test, y_train
-
-
+return X_train, X_test, y_train
